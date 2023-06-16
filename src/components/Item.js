567a7d5c10d2ff39effd0427813057e82b62d42e
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-06-16 15:56:32 -0400

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,42 @@
 import React from "react";
 
-function Item({ item }) {
-  return (
-    <li className={item.isInCart ? "in-cart" : ""}>
-      <span>{item.name}</span>
-      <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-      <button className="remove">Delete</button>
-    </li>
-  );
+
+
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+
+  function handleDeleteClick(event) {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then((item) => onDeleteItem(item));
+  }
+
+
+function handleAddToCartClick(e) {
+  fetch(`http://localhost:4000/items/${item.id}`, {
+
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ isInCart: !item.isInCart, })
+  })
+    .then(res => res.json())
+    .then(updatedItem => onUpdateItem(updatedItem))
+}
+
+return (
+  <li className={item.isInCart ? "in-cart" : ""}>
+    <span>{item.name}</span>
+    <span className="category">{item.category}</span>
+    <button className={item.isInCart ? "remove" : "add"} onClick={handleAddToCartClick}>
+      {item.isInCart ? "Remove From" : "Add to"} Cart
+    </button>
+    <button className="remove" onClick={handleDeleteClick}>Delete</button>
+  </li>
+);
 }
 
 export default Item;
